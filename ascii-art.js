966a5722b7310478525fa8c24e1803a85748e548
--- <118639916+rawbit1@users.noreply.github.com>
2024-10-15 11:19:10 +0200

    clean up of ascii only for needed

--- ascii-art.js
+++ ascii-art.js
@@ -1,16 +1,11 @@
 const asciiArtPaths = {
   pushups: "ascii-art/pushups.txt",
   pullups: "ascii-art/pullups.txt",
-  dumbbellrows: "ascii-art/dumbbellrows.txt",
+  single_arm_dumbell_rows: "ascii-art/dumbbell-rows.txt",
   lunges: "ascii-art/lunges.txt",
-  plank: "ascii-art/plank.txt", // Note: If your file is "plank.txt", keep the key as "plank"
+  plank: "ascii-art/plank.txt",
   squats: "ascii-art/squats.txt",
   hyperextensions: "ascii-art/hyperextensions.txt",
   logo: "ascii-art/logo.txt",
-  boxjumps: "ascii-art/box-jumps.txt",
-  plyopushup: "ascii-art/plyometric-pushup.txt",
-  singlelegromdeadlift: "ascii-art/single-leg-romanian-deadlift.txt",
-  skaterhops: "ascii-art/skater-hops.txt",
-  ballslams: "ascii-art/ball-slams.txt",
-  // Add more mappings as needed
+  single_leg_deadlift: "ascii-art/single-leg-deadlift.txt",
 };
